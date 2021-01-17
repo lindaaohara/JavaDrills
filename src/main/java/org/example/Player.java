5f3507cbb22e695d7f26ecfedcc8310cb18d0c37
Linda <lindaaohara@gmail.com>
2021-01-17 00:56:31 -0500

    playing around with Player

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,116 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Player {
+    private String name;
+    private Integer number;
+    private String position;
+    private Double height;
+    private Double shootingAverage;
+    private Integer salary;
+
+
+    public Player(String name, Integer number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public Map<Integer,String > playerPosition= new HashMap<>();
+    public Map<Integer, Double> playerHeight= new HashMap<>();
+    public Map<Integer, Double> playerShootingAverage = new HashMap<>();
+    public Map<Integer, Integer> playerSalary = new HashMap<>();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getShootingAverage() {
+        return shootingAverage;
+    }
+
+    public void setShootingAverage(Double shootingAverage) {
+        this.shootingAverage = shootingAverage;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Map<Integer, String> getPlayerPosition() {
+        return playerPosition;
+    }
+
+    public void setPlayerPosition(Map<Integer, String> playerPosition) {
+        this.playerPosition = playerPosition;
+    }
+
+    public Map<Integer, Double> getPlayerHeight() {
+        return playerHeight;
+    }
+
+    public void setPlayerHeight(Map<Integer, Double> playerHeight) {
+        this.playerHeight = playerHeight;
+    }
+
+    public Map<Integer, Double> getPlayerShootingAverage() {
+        return playerShootingAverage;
+    }
+
+    public void setPlayerShootingAverage(Map<Integer, Double> playerShootingAverage) {
+        this.playerShootingAverage = playerShootingAverage;
+    }
+
+    public Map<Integer, Integer> getPlayerSalary() {
+        return playerSalary;
+    }
+
+    public void setPlayerSalary(Map<Integer, Integer> playerSalary) {
+        this.playerSalary = playerSalary;
+    }
+
+    public boolean madeShot(){
+        Random rand = new Random();
+        Double random = rand.nextDouble();
+        if(this.shootingAverage>=random) return true;
+        return false;
+    }
+ //   public void shoot2()throws MissedShotException{
+
+}
---
+++ src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    private static Player testPlayer = new Player("Tariq", 1);
+    private static Map<Integer,String > playerPosition= Collections.singletonMap(1, "Guard");
+    private static Map<Integer, Double> playerHeight= Collections.singletonMap(1,6.25);
+    private static Map<Integer, Double> playerShootingAverage = Collections.singletonMap(1,0.5);
+    private static Map<Integer, Integer> playerSalary = Collections.singletonMap(1, 25000000);
+
+    Player player;
+
+
+    @Test
+    public void getName() {
+        String expected = "Tariq";
+        String actual = testPlayer.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumber() {
+        int expected = 1;
+        int actual = testPlayer.getNumber();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPosition() {
+        String expected = "Guard";
+        String actual = playerPosition.get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHeight() {
+        Double expected = 6.25;
+        Double actual = playerHeight.get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getShootingAverage() {
+        Double expected = 0.5;
+        Double actual = playerShootingAverage.get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getSalary() {
+        int expected = 25000000;
+        int actual = playerSalary.get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void madeShot() {
+        Double random = 0.3;
+        Assert.assertTrue(playerShootingAverage.get(1)>=random);
+
+
+    }
+}
